2be0a98a578cd2ff0d54d01f68295c6ca4cd2a4d
kunani1 <131540652+kunani1@users.noreply.github.com>
2024-10-07 19:32:52 +0530

    Update tataplay.php

--- tataplay.php
+++ tataplay.php
@@ -1,25 +1,27 @@
 <?php
 
 $url = 'https://babel-in.xyz/tata/channels';
-$data = [];  // No data is sent in the body, based on your original code
+$data = [
+    'X-API-Key' => 'babel-072c9452f5c5ea9e15a624f487',
+];
+$jsonData = json_encode($data);
 
 $options = [
     'http' => [
         'header'  => [
-            "Content-Type: application/json",
-            "User-Agent: Babel/5.0",
-            "X-API-Key: babel-072c9452f5c5ea9e15a624f487"  // API key added in header
+            "Content-type: application/json",
+            "User-Agent: Babel/5.0"
         ],
         'method'  => 'POST',
-        'content' => json_encode($data),  // Empty array is being encoded into JSON
+        'content' => $jsonData,
     ],
 ];
 
 $context  = stream_context_create($options);
 $response = file_get_contents($url, false, $context);
-
-if ($response === FALSE) {
+$dat= json_encode($response)
+if ($dat === FALSE) {
     die('Error sending request');
 }
 
-echo $response;
+echo $dat;
