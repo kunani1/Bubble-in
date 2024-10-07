3d9a82437834787a89794cc73ed76253c4f9637e
kunani1 <131540652+kunani1@users.noreply.github.com>
2024-10-07 19:36:42 +0530

    Update tataplay.php

--- tataplay.php
+++ tataplay.php
@@ -4,7 +4,7 @@ $url = 'https://babel-in.xyz/tata/channels';
 $data = [
     'X-API-Key' => 'babel-072c9452f5c5ea9e15a624f487',
 ];
-$jsonData = json_encode($data);
+$jsonData = json_decode($data);
 
 $options = [
     'http' => [
@@ -19,7 +19,8 @@ $options = [
 
 $context  = stream_context_create($options);
 $response = file_get_contents($url, false, $context);
-$dat= json_encode($response)
+$dat = json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);  // Added missing semicolon
+
 if ($dat === FALSE) {
     die('Error sending request');
 }
